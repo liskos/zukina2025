c1b0946e84b123a274e02a2737a3a1f6e9044b0d
User <liskosnik2011@yandex.ru>
2025-07-12 15:58:06 +0300

    12 07 25

---
+++ ege02/122.py
@@ -0,0 +1,6 @@
+for x in 0,1:
+    for y in 0,1:
+        for z in 0,1:
+            f = (not x and y and z) or (not x and not y and z) or (not x and not y and not z)
+            if f:
+                print(x, y, z, "|", int(f))
