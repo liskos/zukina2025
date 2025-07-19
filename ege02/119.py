d7cdf398e0a0d65602f3bcd0af0350efb46790c3
User <liskosnik2011@yandex.ru>
2025-07-19 15:37:36 +0300

    19 07 2025

--- ege02/119.py
+++ ege02/119.py
@@ -2,4 +2,5 @@ for a in 0,1:
     for b in 0,1:
         for c in 0,1:
             f = (a and not c) or (not a and b and c)
-            print(a, b, c, "|", int(f))
+            if f:
+                print(a, b, c, "|", int(f))
--- ege02/144.py
+++ ege02/144.py
@@ -3,4 +3,5 @@ for x  in 0,1:
         for z in 0,1:
             for w in 0, 1:
                 f = x and (z and not w or y and not w  or y and not z)
-                print(y, w, x, z, "|", int(f))
+                if f:
+                    print(y, w, x, z, "|", int(f))
